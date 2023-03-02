8ad83f6c1c7fd0d0e93649b95554f54f04cbf282
Valentin <vmihaila333@gmail.com>
2023-03-02 20:41:13 +0200

    trying something different

--- js/index.js
+++ js/index.js
@@ -2,3 +2,14 @@ function showNavList(){
     document.querySelector('#nav-list').classList.toggle('hidden')
 }
 document.querySelector('#nav-bar-icon').addEventListener('click', showNavList)
+
+//some internet magic
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+//some internet magic
