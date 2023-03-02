13e5e337f9795a38e91b198570895c1aab45bdaa
Valentin <vmihaila333@gmail.com>
2023-03-02 20:48:13 +0200

    smol fix

--- js/index.js
+++ js/index.js
@@ -3,13 +3,4 @@ function showNavList(){
 }
 document.querySelector('#nav-bar-icon').addEventListener('click', showNavList)
 
-//some internet magic
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
-
-//some internet magic
